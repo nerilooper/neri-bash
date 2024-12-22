1c35594c434f4faecee80f807046e54b72b3e362
Neriya Rosner <nrosner@doorloop.com>
2024-12-22 15:19:03 +0200

    update script

--- bugsnag.sh
+++ bugsnag.sh
@@ -1,4 +1,3 @@
-
 # Open the Bugsnag dashboard with a specific time range.
 # Usage: bugsnag_open <start_date> <relative_end / end_date>
 # Example: bugsnag_open 2020-01-01T00:00:00.000Z 1d
@@ -10,7 +9,7 @@ alias bugsnag_open='function() {
 
     start_date=$(echo "$start_date" | sed -E "s/\.[0-9]+Z$/Z/");
 
-    if [[ "$relative_end" =~ ^[0-9]+[dh]$ ]]; then
+    if [[ "$relative_end" =~ ^[0-9]+[dhm]$ ]]; then
         local unit="${relative_end: -1}";
         local value="${relative_end%${unit}}";
 
@@ -18,10 +17,16 @@ alias bugsnag_open='function() {
             end_date=$(date -u -v+${value}d -j -f "%Y-%m-%dT%H:%M:%SZ" "${start_date}" +"%Y-%m-%dT%H:%M:%SZ");
         elif [[ "$unit" == "h" ]]; then
             end_date=$(date -u -v+${value}H -j -f "%Y-%m-%dT%H:%M:%SZ" "${start_date}" +"%Y-%m-%dT%H:%M:%SZ");
+        elif [[ "$unit" == "m" ]]; then
+            end_date=$(date -u -v+${value}M -j -f "%Y-%m-%dT%H:%M:%SZ" "${start_date}" +"%Y-%m-%dT%H:%M:%SZ");
         fi
     else
-        echo "Invalid relative end time format. Use something like '1d' or '6h'."
-        return 1;
+        if [[ "$relative_end" =~ ^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}Z$ ]]; then
+            end_date="$relative_end";
+        else
+            echo "Invalid end date format. Use a relative time like '1d', '6h', '30m', or a valid ISO date."
+            return 1;
+        fi
     fi
 
     local full_url="${base_url}${start_date}";
