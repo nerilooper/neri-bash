40ed8846b8694e5b6c16592d20cc438ce3362cca
Neriya Rosner <nrosner@doorloop.com>
2025-04-16 19:15:43 +0300

    update script

--- aliases.sh
+++ aliases.sh
@@ -1,5 +1,5 @@
 # Project paths
-export DOORLOOP_PATH="$HOME/WebstormProjects/doorloop"
+export DOORLOOP_PATH=$(pwd)
 export SERVER_PATH="$DOORLOOP_PATH/server"
 export CLIENT_PATH="$DOORLOOP_PATH/client"
 
@@ -20,16 +20,26 @@ alias pullall="git fetch --all && git pull --all"
 
 alias storybook="cd $CLIENT_PATH && pnpm run storybook"
 
+alias docker-debug="pnpm docker-bootstrap 651d09f9a3895d22c843074a && USE_DOCKER=true pnpm debug-dev"
+
 servertest() {
     DYNAMIC_TEST_PATH="**/*.test.ts"
+    TEST_NAME_FILTER=""
 
+    # First arg filters test files
     if [ ! -z "$1" ]; then
         DYNAMIC_TEST_PATH="**/*$1*.test.ts"
     fi
 
-    CMD="NODE_OPTIONS=--max-old-space-size=65536 node $DOORLOOP_PATH/node_modules/jest/bin/jest.js -c $SERVER_PATH/jest.config.js --runInBand $DYNAMIC_TEST_PATH"
+    # Second arg filters by test name
+    if [ ! -z "$2" ]; then
+        TEST_NAME_FILTER="-t \"$2\""
+    fi
+
+    CMD="NODE_OPTIONS=\"--max-old-space-size=65536 --experimental-vm-modules\" node $DOORLOOP_PATH/node_modules/jest/bin/jest.js -c $SERVER_PATH/jest.config.ts --runInBand $DYNAMIC_TEST_PATH $TEST_NAME_FILTER"
     
     cd $SERVER_PATH
     echo $CMD
     eval $CMD
+    cd $DOORLOOP_PATH
 }
