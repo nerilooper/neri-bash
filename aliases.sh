65151c85b3c06df44a752265a2c75503cc4dce39
Neriya Rosner <nrosner@doorloop.com>
2025-05-20 11:57:25 +0300

    servertest support of libs

--- aliases.sh
+++ aliases.sh
@@ -1,8 +1,13 @@
 # Project paths
-export DOORLOOP_PATH=$(pwd)
-export SERVER_PATH="$DOORLOOP_PATH/server"
-export CLIENT_PATH="$DOORLOOP_PATH/client"
+if [[ "$(pwd)" == *"/server"* ]]; then
+    export SERVER_PATH=$(pwd)
+    export DOORLOOP_PATH="$(dirname "$SERVER_PATH")"
+else
+    export DOORLOOP_PATH=$(pwd)
+    export SERVER_PATH="$DOORLOOP_PATH/apps/server"
+fi
 
+export CLIENT_PATH="$DOORLOOP_PATH/apps/client"
 
 alias stricttoggle="sed -e 's/\"strictNullChecks\": true/\"strictNullChecks\": false/;s/\"strictNullChecks\": false/\"strictNullChecks\": true/' $SERVER_PATH/tsconfig.json > $SERVER_PATH/tsconfig.json.tmp && mv $SERVER_PATH/tsconfig.json.tmp $SERVER_PATH/tsconfig.json"
 alias stricton="sed -i '' 's/\"strictNullChecks\": false/\"strictNullChecks\": true/' $SERVER_PATH/tsconfig.json"
@@ -25,20 +30,40 @@ alias docker-debug="pnpm docker-bootstrap 651d09f9a3895d22c843074a && USE_DOCKER
 servertest() {
     DYNAMIC_TEST_PATH="**/*.test.ts"
     TEST_NAME_FILTER=""
+    TARGET_PATH="$SERVER_PATH"
+    JEST_CONFIG="$SERVER_PATH/jest.config.ts"
 
-    # First arg filters test files
-    if [ ! -z "$1" ]; then
-        DYNAMIC_TEST_PATH="**/*$1*.test.ts"
+    # Check for --lib flag
+    if [ "$1" = "--lib" ]; then
+        if [ -z "$2" ]; then
+            echo "Usage: servertest --lib <libname> [test-file-filter] [test-name-filter]"
+            return 1
+        fi
+        LIB_NAME="$2"
+        TARGET_PATH="$DOORLOOP_PATH/libs/$LIB_NAME"
+        JEST_CONFIG="$TARGET_PATH/jest.config.ts"
+        shift 2
+        # Now $1 is test-file-filter, $2 is test-name-filter
+        if [ ! -z "$1" ]; then
+            DYNAMIC_TEST_PATH="**/*$1*.test.ts"
+        fi
+        if [ ! -z "$2" ]; then
+            TEST_NAME_FILTER="-t \"$2\""
+        fi
+    else
+        # First arg filters test files
+        if [ ! -z "$1" ]; then
+            DYNAMIC_TEST_PATH="**/*$1*.test.ts"
+        fi
+        # Second arg filters by test name
+        if [ ! -z "$2" ]; then
+            TEST_NAME_FILTER="-t \"$2\""
+        fi
     fi
 
-    # Second arg filters by test name
-    if [ ! -z "$2" ]; then
-        TEST_NAME_FILTER="-t \"$2\""
-    fi
+    CMD="NODE_OPTIONS=\"--max-old-space-size=65536 --experimental-vm-modules\" node $DOORLOOP_PATH/node_modules/jest/bin/jest.js -c $JEST_CONFIG --runInBand $DYNAMIC_TEST_PATH $TEST_NAME_FILTER"
 
-    CMD="NODE_OPTIONS=\"--max-old-space-size=65536 --experimental-vm-modules\" node $DOORLOOP_PATH/node_modules/jest/bin/jest.js -c $SERVER_PATH/jest.config.ts --runInBand $DYNAMIC_TEST_PATH $TEST_NAME_FILTER"
-    
-    cd $SERVER_PATH
+    cd $TARGET_PATH
     echo $CMD
     eval $CMD
     cd $DOORLOOP_PATH
